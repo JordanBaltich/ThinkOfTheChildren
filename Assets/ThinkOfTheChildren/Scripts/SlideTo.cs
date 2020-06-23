d98e974e02eb07dd0ac8ef93240f337a35f7c863
Pamela Payad <payadpamela@gmail.com>
2020-06-22 22:32:18 -0400

    Tabs can open and close

---
+++ Assets/ThinkOfTheChildren/Scripts/SlideTo.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideTo : MonoBehaviour
+{
+
+    public GameObject folder;
+    public Transform openPos;
+    public Transform closePos;
+
+    public GameObject exitButton;
+
+    public bool tabsIsOpen;
+
+    public float speed;
+
+    private void Start()
+    {
+        exitButton.SetActive(false);
+        tabsIsOpen = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            Debug.Log("pressed");
+            if (tabsIsOpen)
+            {
+                Close();
+            }
+            else if(tabsIsOpen == false)
+            {
+                Open();
+            }
+        }
+    }
+
+    public void SpawnCloseArea()
+    {
+        exitButton.SetActive(true);
+    }
+
+    public void Open()
+    {
+        tabsIsOpen = true;
+        SpawnCloseArea();
+        transform.position = openPos.position;
+        
+    }
+
+    public void Close()
+    {
+        tabsIsOpen = false;
+        exitButton.SetActive(false);
+        transform.position = closePos.position;
+    }
+}
