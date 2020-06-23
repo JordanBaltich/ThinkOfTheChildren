ab6b23152d3391d299ac196077c5a046703f119b
Shane <48694586+ShaneLogtenberg@users.noreply.github.com>
2020-06-23 09:30:05 -0400

    Updated Journal
    Updated Journal to only take info once. A Debug.Log is shows if the info was already added.
    Updated NewMarket to remove the condition that was keeping the info from being added.

--- Assets/AdventureSampleGame/Scripts/MonoBehaviours/Inventory/Journal.cs
+++ Assets/AdventureSampleGame/Scripts/MonoBehaviours/Inventory/Journal.cs
@@ -17,16 +17,22 @@ public class Journal : MonoBehaviour
     }
 
 
-    // This function is called by the PickedUpItemReaction in order to add an item to the inventory.
+    // This function is called by the JournalReaction in order to add an imfo to the journal.
     public void MakeNoteOnKeyInfo(KeyInfo infoToAdd)
     {
         // Go through all the item slots...
         for (int i = 0; i < keyInfo.Length; i++)
         {
-            // ... if the item slot is empty...
+            // ... if the info is already there
+            if (keyInfo[i] == infoToAdd)
+            {
+                Debug.Log("Info Already Added");
+                return;
+            }
+            // ... if the info slot is empty...
             if (keyInfo[i] == null)
             {
-                // ... set it to the picked up item and set the image component to display the item's sprite.
+                // ... set it to the picked up info and set the text component to display the info text string.
                 keyInfo[i] = infoToAdd;
                 infoDescription[i].text = infoToAdd.Description;
                 infoDescription[i].enabled = true;
@@ -36,14 +42,14 @@ public class Journal : MonoBehaviour
     }
 
 
-    // This function is called by the LostItemReaction in order to remove an item from the inventory.
-    public void ScrapNote (KeyInfo itemToRemove)
+    // This function is called by the LostItemReaction in order to remove an imfo to the journal.
+    public void ScrapNote (KeyInfo infoToRemove)
     {
         // Go through all the item slots...
         for (int i = 0; i < keyInfo.Length; i++)
         {
             // ... if the item slot has the item to be removed...
-            if (keyInfo[i] == itemToRemove)
+            if (keyInfo[i] == infoToRemove)
             {
                 // ... set the item slot to null and set the image component to display nothing.
                 keyInfo[i] = null;
