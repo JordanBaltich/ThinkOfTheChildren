50194e6a46acea8bfbf8309e6205c21ece0529a3
Pamela Payad <payadpamela@gmail.com>
2020-07-24 13:18:09 -0400

    Started notification thingy

---
+++ Assets/ChildProtection/Scripts/Editor/Interaction/ReactionEditors/NotificationReactionEditor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(NotificationReaction))]
+public class NotificationReactionEditor : ReactionEditor
+{
+    protected override string GetFoldoutLabel()
+    {
+        return "Notification Reaction";
+    }
+}
