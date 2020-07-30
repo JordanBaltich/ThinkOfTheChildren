f5ee16c33050accefaea538b43b57c2a0766ea43
darrylfeniquito <48287066+darrylfeniquito@users.noreply.github.com>
2020-07-30 10:19:35 -0400

    Revert "Merge branch 'SubMaster' of https://github.com/JordanBaltich/ThinkOfTheChildren into SubMaster"
    This reverts commit 0086c73f3090a15a6d1b2dcf64958907b73472e3, reversing
    changes made to a0c979473ea00b667cd4e3a7dbc35da84a4611db.

--- Assets/ChildProtection/Scripts/UI/Testing/InitialVisit.cs
+++ Assets/ChildProtection/Scripts/UI/Testing/InitialVisit.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InitialVillageVisit : MonoBehaviour
+public class InitialVisit : MonoBehaviour
 {
     public ReactionCollection reaction;
+    public static InitialVisit Instance;
 
     // Start is called before the first frame update
     void Awake()
     {
+        if (Instance != null) { Destroy(gameObject); return; } // stops dups running
+        DontDestroyOnLoad(gameObject); // keep me forever
+        Instance = this; // set the reference to it
+
         StartCoroutine(WaitToStartTutorial());
     }
 
